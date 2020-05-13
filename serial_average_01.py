35d362a4ed55268daa149d0d38e3df64f3b36051
Jeff Alexander <jefalexa@gmail.com>
2020-05-13 09:22:17 -0600

    changed average to list

--- serial_average_01.py
+++ serial_average_01.py
@@ -8,8 +8,7 @@ logging.basicConfig(filename='co2mon.log', level=logging.INFO)
 
 ser = serial.Serial(port='/dev/ttyUSB0', baudrate = 9600, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=1)
 
-counter=0
-weight_avg = 0
+wlist = []
 	
 while 1:
 	x = ser.readline()
@@ -17,17 +16,12 @@ while 1:
 	z = y.split(",")[0]
 	try:
 		weight = float(z.split("b'")[1])
-		if weight_avg == 0:
-			weight_avg = weight
-		weight_avg = (weight_avg + weight) / 2
-		#print(weight, round(weight_avg, 2))
-		counter += 1
+		wlist.append(weight)
 	except:
 		print(y)
-	if counter >= 60:
-		dt = datetime.datetime.now()
-		dt2 = dt.strftime("%Y-%m-%d_%H-%M")
-		print("{}:  1 Minute Average:  {}".format(dt2, round(weight_avg, 2)))
+	if len(wlist) >= 60:
+		weight_avg = sum(wlist) / len(wlist)
+		dt = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
+		print("{}:  1 Minute Average:  {}".format(dt, round(weight_avg, 2)))
 		logging.log(msg="{}:  1 Minute Average:  {}".format(dt2, round(weight_avg, 2)), level=logging.INFO)
-		counter = 0
-		weight_avg = 0
+		wlist = []
